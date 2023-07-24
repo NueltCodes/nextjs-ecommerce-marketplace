2dfbf06e9a06863b9585fbbc0c8b6c34aa6616ce
NueltCodes <olaniranemmanuelt@gmail.com>
2023-07-24 20:43:51 +0100

    1a. Created the routes/actions in getting products, category, billboards to display in the frontend pages and components. currently modifying some of the page and and adding components.

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,7 +1,24 @@
+import getBillboard from "@/actions/get-billboard";
+import getProducts from "@/actions/get-products";
+import Billboard from "@/components/billboard";
+import ProductList from "@/components/product-list";
+import Container from "@/components/ui/container";
 import React from "react";
 
-const HomePage = () => {
-  return <div>Hello Store</div>;
+export const revalidate = 0;
+const HomePage = async () => {
+  const billboard = await getBillboard("ba8a9895-ba47-4358-90b7-361ffe81e639");
+  const products = await getProducts({ isFeatured: true });
+  return (
+    <Container>
+      <div className="space-y-10 pb-10">
+        <Billboard data={billboard} />
+      </div>
+      <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
+        <ProductList title="Featured Products" items={products} />
+      </div>
+    </Container>
+  );
 };
 
 export default HomePage;
