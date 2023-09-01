487c8a66a6421e2e25027d36a402360a04d14b06
Olaniran Emmanuel <114101461+NueltCodes@users.noreply.github.com>
2023-09-01 04:57:06 +0100

    Update get-newProducts.tsx

--- actions/get-newProducts.tsx
+++ actions/get-newProducts.tsx
@@ -3,46 +3,42 @@ import qs from "query-string";
 
 const URL = `${process.env.NEXT_PUBLIC_API_URL}/products`;
 
+import { Product } from "@/types";
+import qs from "query-string";
+
+const URL = `${process.env.NEXT_PUBLIC_API_URL}/products`;
+
 interface Query {
-  categoryId?: string;
-  colorId?: string;
-  sizeId?: string;
   isFeatured?: boolean;
 }
 
-const getNewProducts = async (query: Query): Promise<Product[]> => {
-  // Filter out undefined query parameters
-  const filteredQuery: qs.StringifiableRecord = {};
-  if (query.colorId) filteredQuery.colorId = query.colorId;
-  if (query.sizeId) filteredQuery.sizeId = query.sizeId;
-  if (query.categoryId) filteredQuery.categoryId = query.categoryId;
-  if (query.isFeatured !== undefined) {
-    // Convert boolean to string cause the query expects all to be string so as to avoid errors
-    filteredQuery.isFeatured = query.isFeatured.toString();
-  }
-
-  const url = qs.stringifyUrl({
-    url: URL,
-    query: filteredQuery,
-  });
+const getNewArrivals = async (query: Query = {}): Promise<Product[]> => {
+  const sortedQuery = {
+    ...query,
+    _sort: "createdAt:desc", // Sort by createdAt in descending order
+    _limit: 1, // Limit the number of results to 20
+  };
+  // ...
 
   try {
-    // Append a timestamp to the URL to avoid caching
+    const url = qs.stringifyUrl({
+      url: URL,
+      query: sortedQuery,
+    });
+
     const timestamp = Date.now();
     const res = await fetch(`${url}&timestamp=${timestamp}`);
 
-    // Check if the response is successful (status code within the range of 200-299)
     if (!res.ok) {
-      // If the response is not successful, throw an error
-      throw new Error("Failed to fetch product data");
+      throw new Error("Failed to fetch new arrival products");
     }
 
     return res.json();
   } catch (error) {
-    // Handle network errors or errors during JSON parsing
-    console.error("Error fetching products:", error);
+    console.error("Error fetching new arrival products:", error);
     throw error;
   }
 };
+
 
 export default getNewProducts;
